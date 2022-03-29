a3af91bfc0d4021f90a186bd4a121b0dc711aadf
_o <choijuo@naver.com>
2022-03-29 17:04:19 +0900

    Create Lv1_내적.java

---
+++ programmers/Lv1_내적.java
@@ -0,0 +1,13 @@
+package lv1;
+
+public class Lv1_³»Àû {
+	public int solution(int[] a, int[] b) {
+        int answer = 0;
+        
+        for(int i=0;i<a.length;i++){
+            answer+=a[i]*b[i];
+        }
+        
+        return answer;
+    }
+}
