70d8f6e18e7737a73c3be5eaf7a044d8b46be63d
_o <choijuo@naver.com>
2022-03-29 17:04:41 +0900

    Create Lv1_완주하지못한선수Hash.java

---
+++ programmers/Lv1_완주하지못한선수Hash.java
@@ -0,0 +1,40 @@
+package lv1;
+
+//import java.util.HashMap;
+import java.util.*;
+public class Lv1_완주하지못한선수Hash {
+	public String solution(String[] participant,String[] completion) {
+		String answer="";
+		HashMap<String, Integer> hm=new HashMap<>();
+		
+		for(String player:participant) {
+			int cnt=hm.getOrDefault(player, 0);
+			hm.put(player,++cnt);
+			
+		}
+		System.out.println(hm.toString());
+		
+		for(String player:completion) {
+			hm.put(player, hm.get(player)-1);
+		}
+		
+		
+		System.out.println(hm.toString());
+		
+		
+		
+		
+		return answer;
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String []participant= {"mislav","stanko","mislav","ana"};
+		String []completion= {"stanko","ana","mislav"};
+		
+		Lv1_완주하지못한선수Hash ph=new Lv1_완주하지못한선수Hash();
+		ph.solution(participant, completion);
+
+	}
+
+}
