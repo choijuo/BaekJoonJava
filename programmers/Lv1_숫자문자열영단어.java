3e734de2d743db3fbe582e1f9632051ae6d2916a
_o <choijuo@naver.com>
2022-03-29 17:04:24 +0900

    Create Lv1_숫자문자열영단어.java

---
+++ programmers/Lv1_숫자문자열영단어.java
@@ -0,0 +1,15 @@
+package lv1;
+
+public class Lv1_숫자문자열영단어 {
+	public int solution(String s) {
+        
+        int answer = 0;
+        String[] numArr={"0","1","2","3","4","5","6","7","8","9"};
+        String[] wordArr={"zero","one","two","three","four","five","six","seven","eight","nine"};
+        for(int i=0;i<10;i++){
+            s=s.replaceAll(wordArr[i],numArr[i]);
+        }
+        answer=Integer.parseInt(s);
+        return answer;
+    }
+}
